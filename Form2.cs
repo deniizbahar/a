d56e4750ad4eb9bcce574481a1d6db53a737bce2
Deniz Bahar <105117531+deniizbahar@users.noreply.github.com>
2024-07-20 17:41:00 +0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows.Forms;
+using Günlük;
+using Microsoft.Web.Administration;
+using Serilog.Events;
+
+namespace MainForm
+{
+    public partial class Form2 : Form
+    {
+        public static event EventHandler<ConfigChangedEventArgs> ConfigChanged;
+
+        private string controlServiceName = "ControlService";
+        private string mockServiceName = "MockService";
+        private string appPoolName = "mywebapipool";
+
+        public Form2()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+            LoggerHelper.ConfigureLogger(new LoggerService("C://Logs/form-config-changes-log.json", LogEventLevel.Information));
+        }
+
+        private void InitializeComboBoxes()
+        {
+            cbControlServiceLogLevel.Items.AddRange(Enum.GetNames(typeof(LogLevel)));
+            cbWebApiLogLevel.Items.AddRange(Enum.GetNames(typeof(LogLevel)));
+            cbMockServiceLogLevel.Items.AddRange(Enum.GetNames(typeof(LogLevel)));
+
+            // Varsayılan değerleri seçin
+            cbControlServiceLogLevel.SelectedIndex = 2; // Information
+            cbWebApiLogLevel.SelectedIndex = 2; // Information
+            cbMockServiceLogLevel.SelectedIndex = 2; // Information
+        }
+
+        private void btnStartControlService_Click(object sender, EventArgs e)
+        {
+            StartService(controlServiceName, lblControlServiceStatus);
+            var config = new ServiceConfigDto { Application = ApplicationType.ControlService, LogLevel = GetSelectedLogLevel(cbControlServiceLogLevel) };
+            OnConfigChanged(config);
+        }
+
+        private void btnStopControlService_Click(object sender, EventArgs e)
+        {
+            StopService(controlServiceName, lblControlServiceStatus);
+        }
+
+        private void btnStartMockService_Click(object sender, EventArgs e)
+        {
+            StartService(mockServiceName, lblMockServiceStatus);
+            var config = new ServiceConfigDto { Application = ApplicationType.MockService, LogLevel = GetSelectedLogLevel(cbMockServiceLogLevel), MonitoringFrequency = int.Parse(txtMockServiceFrequency.Text) };
+            OnConfigChanged(config);
+        }
+
+        private void btnStopMockService_Click(object sender, EventArgs e)
+        {
+            StopService(mockServiceName, lblMockServiceStatus);
+        }
+
+        private void btnStartAppPool_Click(object sender, EventArgs e)
+        {
+            StartAppPool(appPoolName, lblAppPoolStatusIndicator);
+            var config = new ServiceConfigDto { Application = ApplicationType.mywebapipool, LogLevel = GetSelectedLogLevel(cbWebApiLogLevel), MonitoringFrequency = int.Parse(txtWebApiFrequency.Text) };
+            OnConfigChanged(config);
+        }
+
+        private void btnStopAppPool_Click(object sender, EventArgs e)
+        {
+            StopAppPool(appPoolName, lblAppPoolStatusIndicator);
+        }
+
+        private void btnSaveSettings_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OnConfigChanged(new ServiceConfigDto
+                {
+                    Application = ApplicationType.ControlService,
+                    LogLevel = GetSelectedLogLevel(cbControlServiceLogLevel)
+                });
+
+                OnConfigChanged(new ServiceConfigDto
+                {
+                    Application = ApplicationType.mywebapipool,
+                    LogLevel = GetSelectedLogLevel(cbWebApiLogLevel),
+                    MonitoringFrequency = int.Parse(txtWebApiFrequency.Text)
+                });
+
+                OnConfigChanged(new ServiceConfigDto
+                {
+                    Application = ApplicationType.MockService,
+                    LogLevel = GetSelectedLogLevel(cbMockServiceLogLevel),
+                    MonitoringFrequency = int.Parse(txtMockServiceFrequency.Text)
+                });
+
+                MessageBox.Show("Ayarlar kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ayarlar kaydedilirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartService(string serviceName, Label statusLabel)
+        {
+            try
+            {
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Stopped)
+                {
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running);
+                    statusLabel.Text = "Başlatıldı";
+                }
+                else
+                {
+                    statusLabel.Text = "Zaten çalışıyor";
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                statusLabel.Text = $"Yönetici hakları gereklidir: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+        }
+
+        private void StopService(string serviceName, Label statusLabel)
+        {
+            try
+            {
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                    statusLabel.Text = "Durduruldu";
+                }
+                else
+                {
+                    statusLabel.Text = "Zaten durdurulmuş";
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                statusLabel.Text = $"Yönetici hakları gereklidir: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+        }
+
+        private void StartAppPool(string appPoolName, Label statusLabel)
+        {
+            try
+            {
+                using (ServerManager serverManager = new ServerManager())
+                {
+                    ApplicationPool appPool = serverManager.ApplicationPools[appPoolName];
+                    if (appPool != null && appPool.State == ObjectState.Stopped)
+                    {
+                        appPool.Start();
+                        statusLabel.Text = "Başlatıldı";
+                    }
+                    else
+                    {
+                        statusLabel.Text = "Zaten çalışıyor";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+        }
+
+        private void StopAppPool(string appPoolName, Label statusLabel)
+        {
+            try
+            {
+                using (ServerManager serverManager = new ServerManager())
+                {
+                    ApplicationPool appPool = serverManager.ApplicationPools[appPoolName];
+                    if (appPool != null && appPool.State == ObjectState.Started)
+                    {
+                        appPool.Stop();
+                        statusLabel.Text = "Durduruldu";
+                    }
+                    else
+                    {
+                        statusLabel.Text = "Zaten durdurulmuş";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = $"Hata: {ex.Message}";
+            }
+        }
+
+       
+
+        protected virtual void OnConfigChanged(ServiceConfigDto newConfig)
+        {
+            if (ConfigChanged != null)
+            {
+                // Dinleyici varsa
+                LoggerHelper.ConfigureLogger(new LoggerService("C://Logs/event_subscribed_log.json", LogEventLevel.Information));
+                LoggerHelper.Log(LogEventLevel.Information, $"ConfigChanged event triggered with new config: {newConfig.Application}, LogLevel: {newConfig.LogLevel}, MonitoringFrequency: {newConfig.MonitoringFrequency}");
+                ConfigChanged.Invoke(this, new ConfigChangedEventArgs { NewConfig = newConfig });
+            }
+            else
+            {
+                // Dinleyici yoksa
+                LoggerHelper.ConfigureLogger(new LoggerService("C://Logs/no_event_subscribed_log.json", LogEventLevel.Information));
+                LoggerHelper.Log(LogEventLevel.Information, $"No subscribers for ConfigChanged event. New config: {newConfig.Application}, LogLevel: {newConfig.LogLevel}, MonitoringFrequency: {newConfig.MonitoringFrequency}");
+            }
+        }
+
+        private LogLevel GetSelectedLogLevel(ComboBox comboBox)
+        {
+            return (LogLevel)Enum.Parse(typeof(LogLevel), comboBox.SelectedItem.ToString());
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            InitializeComboBoxes();
+        }
+    }
+}
---
+++ MyService.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using Günlük;
+using MainForm;
+using Serilog.Events;
+
+namespace ControlService
+{
+    public class MyService : ServiceBase
+    {
+        private Task _executingTask;
+        private readonly CancellationTokenSource _stoppingCts = new CancellationTokenSource();
+        private ServiceConfigDto _mywebapipoolConfig = new ServiceConfigDto { Application = ApplicationType.mywebapipool, LogLevel = LogLevel.Information, MonitoringFrequency = 10 };
+        private ServiceConfigDto _mockServiceConfig = new ServiceConfigDto { Application = ApplicationType.MockService, LogLevel = LogLevel.Information, MonitoringFrequency = 10 };
+        private ServiceConfigDto _controlServiceConfig = new ServiceConfigDto { Application = ApplicationType.ControlService, LogLevel = LogLevel.Information };
+
+        public MyService()
+        {
+            this.ServiceName = "ControlService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var initialLogLevel = LogLevelConverter.ToSerilogLevel(_controlServiceConfig.LogLevel);
+            LoggerHelper.ConfigureLogger(new LoggerService("C://Logs/monitoring-log.json", initialLogLevel));
+            LoggerHelper.Log(LogEventLevel.Information, "Control Service started.");
+
+            SubscribeToConfigChanges();
+
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+        }
+
+        protected override void OnStop()
+        {
+            if (_executingTask == null)
+            {
+                return;
+            }
+
+            _stoppingCts.Cancel();
+            _executingTask.Wait();
+            LoggerHelper.Log(LogEventLevel.Information, "Control Service stopped.");
+        }
+
+        private async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    LoggerHelper.Log(LogEventLevel.Information, "Worker running");
+
+                    if (_mockServiceConfig != null && _mockServiceConfig.MonitoringFrequency.HasValue)
+                    {
+                        await CheckAndRestartServiceAsync("MockService", _mockServiceConfig.MonitoringFrequency.Value, stoppingToken);
+                    }
+
+                    if (_mywebapipoolConfig != null && _mywebapipoolConfig.MonitoringFrequency.HasValue)
+                    {
+                        await CheckAndRestartAppPoolAsync("mywebapipool", _mywebapipoolConfig.MonitoringFrequency.Value, stoppingToken);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LoggerHelper.Log(LogEventLevel.Error, $"An error occurred: {ex.Message}");
+                }
+
+                await Task.Delay(1000); // Ana döngüde bekleme süresi (1 saniye) ekleyin
+            }
+        }
+
+        private async Task CheckAndRestartServiceAsync(string serviceName, int frequency, CancellationToken stoppingToken)
+        {
+            try
+            {
+                using (ServiceController sc = new ServiceController(serviceName))
+                {
+                    if (sc.Status == ServiceControllerStatus.Stopped || sc.Status == ServiceControllerStatus.StopPending)
+                    {
+                        LoggerHelper.Log(LogEventLevel.Warning, $"Service {serviceName} is stopped. Attempting to restart.");
+                        sc.Start();
+                        sc.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                        LoggerHelper.Log(LogEventLevel.Information, $"Service {serviceName} restarted successfully.");
+                    }
+                    else
+                    {
+                        LoggerHelper.Log(LogEventLevel.Information, $"Service {serviceName} is running.");
+                    }
+                }
+                await Task.Delay(frequency * 1000, stoppingToken); // Belirtilen aralıklarla kontrol
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.Log(LogEventLevel.Error, $"Error checking or restarting service {serviceName}: {ex.Message}");
+            }
+        }
+
+        private async Task CheckAndRestartAppPoolAsync(string appPoolName, int frequency, CancellationToken stoppingToken)
+        {
+            try
+            {
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"Import-Module WebAdministration; $appPool = Get-Item 'IIS:\\AppPools\\{appPoolName}'; if ($appPool.state -eq 'Stopped') {{ Start-WebAppPool -Name '{appPoolName}'; Write-Output 'App Pool {appPoolName} restarted successfully.'; }} else {{ Write-Output 'App Pool {appPoolName} is running.'; }}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = Process.Start(processInfo))
+                {
+                    process.WaitForExit();
+                    var output = process.StandardOutput.ReadToEnd();
+                    var error = process.StandardError.ReadToEnd();
+
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        LoggerHelper.Log(LogEventLevel.Information, output);
+                    }
+
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        LoggerHelper.Log(LogEventLevel.Error, $"Error restarting App Pool {appPoolName}: {error}");
+                    }
+                }
+                await Task.Delay(frequency * 1000, stoppingToken); // Belirtilen aralıklarla kontrol
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.Log(LogEventLevel.Error, $"Error checking or restarting App Pool {appPoolName}: {ex.Message}");
+            }
+        }
+
+        private void SubscribeToConfigChanges()
+        {
+            Form2.ConfigChanged += Form2_ConfigChanged;
+            LoggerHelper.Log(LogEventLevel.Information, "Subscribed to Form2 ConfigChanged event.");
+        }
+
+        private void Form2_ConfigChanged(object sender, ConfigChangedEventArgs e)
+        {
+            LoggerHelper.Log(LogEventLevel.Information, $"ConfigChanged event received: {e.NewConfig.Application}, LogLevel: {e.NewConfig.LogLevel}, MonitoringFrequency: {e.NewConfig.MonitoringFrequency}");
+
+            if (e.NewConfig.Application == ApplicationType.ControlService)
+            {
+                _controlServiceConfig = e.NewConfig;
+                var newLogLevel = LogLevelConverter.ToSerilogLevel(_controlServiceConfig.LogLevel);
+                LoggerHelper.SetLogLevel(newLogLevel);
+                LoggerHelper.Log(LogEventLevel.Information, $"Updated ControlService config: LogLevel = {_controlServiceConfig.LogLevel}");
+            }
+            else if (e.NewConfig.Application == ApplicationType.MockService)
+            {
+                _mockServiceConfig = e.NewConfig;
+                LoggerHelper.Log(LogEventLevel.Information, $"Updated MockService config: Frequency = {_mockServiceConfig.MonitoringFrequency}");
+            }
+            else if (e.NewConfig.Application == ApplicationType.mywebapipool)
+            {
+                _mywebapipoolConfig = e.NewConfig;
+                LoggerHelper.Log(LogEventLevel.Information, $"Updated mywebapipool config: Frequency = {_mywebapipoolConfig.MonitoringFrequency}");
+            }
+        }
+    }
+}
